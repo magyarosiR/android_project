193d60a8dbc46d74af19a157e2f1a45f7dd356e2
Magyarosi Roland <rolandmagyarosi@gmail.com>
2020-11-26 00:34:00 +0200

    splash screen

---
+++ app/src/main/java/rol/myappcompany/android_project/SplashActivity.kt
@@ -0,0 +1,33 @@
+package rol.myappcompany.android_project
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+public class SplashActivity : AppCompatActivity() {
+
+    protected val handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    private val runnable = Runnable {
+        if(!isFinishing){
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable, 2000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+
+}
