91a630443deffd1126974fa49d7b29a3f983b477
Magyarosi Roland <rolandmagyarosi@gmail.com>
2020-11-30 14:07:35 +0200

    profile

--- app/src/main/java/rol/myappcompany/android_project/MainActivity.kt
+++ app/src/main/java/rol/myappcompany/android_project/MainActivity.kt
@@ -3,10 +3,33 @@ package rol.myappcompany.android_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var profileFragment: ProfileFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavitagion: BottomNavigationView = findViewById(R.id.btm_nav)
+
+        bottomNavitagion.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId){
+                R.id.btn_profile -> {
+                    profileFragment = ProfileFragment()
+                    supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.frame_layout, profileFragment)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                            .commit()
+                }
+            }
+            true
+
+        }
+
     }
 }
